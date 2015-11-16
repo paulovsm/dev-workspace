d2c52ce49a7e7441fff4501f71bbfce00c4d5dca
john.smith <john.smith@accenture.com>
2015-11-16 11:20:51 -0300

    Add docker installer script

---
+++ install_docker.ps1
@@ -0,0 +1,14 @@
+﻿$docker_installer_source = "https://github.com/docker/toolbox/releases/download/v1.9.0b/DockerToolbox-1.9.0b.exe"
+$docker_installer_destination = "DockerToolbox-1.9.0b.exe"
+
+If ( -Not (Test-Path -Path $docker_installer_destination) ) {
+    Invoke-WebRequest $docker_installer_source -OutFile $docker_installer_destination 
+} Else {
+    "$docker_installer_destination já existe"
+}
+
+If ( -Not (Test-Path -Path "${env:ProgramFiles(x86)}\Docker Toolbox") ) {
+    & ./${docker_installer_destination} /silent
+} Else {
+    "$docker_installer_destination já instalado"
+}
