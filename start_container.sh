4d5c45949b3e14982e3e049c5dc405b75a753136
john.smith <john.smith@accenture.com>
2015-11-16 17:01:10 -0300

    Updating start up scripts

--- start_container.sh
+++ start_container.sh
@@ -8,7 +8,7 @@ trap finish EXIT
 if [[ "$(sudo docker images -q paulovsm/weblogic 2> /dev/null)" == "" ]]; then
 	sudo docker pull paulovsm/weblogic
 	
-	sudo docker run --name my-weblogic -e WEBLOGIC_PASSWD=weblogic123 -e DOMAIN_DIR=/weblogic/domains -e DOMAIN_NAME=mydomain -e SERVER_START_MODE=dev -p 7001:7001 -p 7002:7002 -ti -v $PWD/teste:/weblogic/wls12130 paulovsm/weblogic
+	sudo docker rm my-weblogic && sudo docker run --name my-weblogic -e WEBLOGIC_PASSWD=weblogic123 -e DOMAIN_DIR=/weblogic/domains -e DOMAIN_NAME=blodes_domain -e SERVER_START_MODE=dev -p 7001:7001 -p 7002:7002 -p 8453:8453 -e "debugFlag=true" -e "EXTRA_JAVA_PROPERTIES=-Duser.timezone='-03:00'" -ti -v /c/Users/Public/wls_servers/spm:/weblogic/wls12130 paulovsm/weblogic  
 else
 	sudo docker start my-weblogic
 fi
